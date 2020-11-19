50f87c54d82ac6e537d86b2fd228edf87155d12a
walanbeel <67581513+walanbeel@users.noreply.github.com>
2020-11-19 15:36:54 +0300

    first commit

---
+++ app/views/product.php
@@ -0,0 +1,245 @@
+<?PHP
+ session_start();
+include "app/views/header.php"; 
+
+?>
+
+
+  <!-- Product Detail Page Start -->
+	<div class="product-detail">
+		<div class="container">
+    
+         <?php 
+             $rows=$data['products'];
+            // echo "<br>". $rows['key']->product_id."<br>";
+            
+           // print_r($rows);
+            foreach($rows as $row)
+            {
+              //echo "<br>". $row->product_id."<br>";
+           if ($row->product_id == $_GET['product_id']){
+              $id = $row->product_id;
+            $mainimageURl = 'http://localhost:81/Ecom-store-project/app/assets/images/'.$row->product_main_image;
+          ?>
+			<div class="product-detail-left">
+				<div class="sp-loading">
+          <img src="/Ecom-store-project/app/assets/images/sp-loading.gif">
+					تحميل الصورة
+				</div>
+			 <div class="sp-wrap">
+        <a href='<?php  echo  $mainimageURl ?>'><img src='<?php echo $mainimageURl ?>' alt=""></a>
+        <?php
+            $imges = $row->product_images;
+            $clean_url=rtrim($imges,',');
+            $clean_url=explode(',',$clean_url);
+            foreach($clean_url as $part){
+            $product_images='http://localhost:81/Ecom-store-project/app/assets/images/'.$part;
+            echo "<a href='$product_images'><img src='  $product_images'></a>"
+            ;}?> 
+				</div>
+			</div>
+			<div class="product-detail-right">
+      
+			<h3><?php  echo $row->product_short_desc ?></h3> 
+				<h5><b>السعر : </b> <i class="fas fa-dollar-sign"></i> $ <?php  echo $row->product_price ?></h5>
+				<h5><b>العلامة التجارية : </b>  <?php  echo $row->brand_name ?></h5>
+				<h5><b>اللون : </b><?php  echo $row->color_name ?></h5>
+				<h5><b>متوفر : </b> <?php if($row->is_active==1)  echo "نعم"; else echo "لا"  ?></h5></h5>
+        <!-- <a  class="Wishlist addtocart" data-tip="Add to Wishlist" data-id='<?= $id; ?>' ><i class="fa fa-heart"></i>  المفضلة</a>
+				<a  class="cart buynow" data-id='<?= $id; ?>'><i class="fa fa-shopping-cart"></i>إضافة لسلة</a> -->
+        <?PHP
+            if(array_key_exists($id, $_SESSION['wish']))
+            {
+            echo "<a style='color:blue' data-tip='Add to Wishlist' data-id='$id' id='wishlisticon' class='Wishlist addtocart'><i class='fa fa-heart'></i>  المفضلة</a>";
+            }
+            else
+            {
+            echo "<a data-tip='Add to Wishlist' data-id='$id' id='wishlisticon' class='Wishlist addtocart'><i class='fa fa-heart'></i>  المفضلة</a>";
+            }
+        ?>
+       <?PHP
+            if(array_key_exists($id, $_SESSION['cart']))
+            {
+            echo "<a style='color:blue' class='cart buynow' data-id='$id' id='shope' ><i class='fa fa-shopping-cart'></i>إضافة لسلة</a>";
+            }
+            else
+            {
+              echo "<a  class='cart buynow' data-id='$id' id='shope' ><i class='fa fa-shopping-cart'></i>إضافة لسلة</a>";
+            }
+        ?>
+			</div>
+      <?php } }?> 
+		</div>
+	
+	<!-- Product Detail Page End -->
+
+
+  
+
+
+  <script>
+$(document).ready(function(){
+            // add the item to cart
+            $('.cart').click(function(){
+              var id = $(this).data('id');
+            // alert(id);
+            $.ajax({
+                url: 'main/shopingCart?id='+ id,
+                method: 'post',
+                cache: false,
+                data: {
+                  id: id
+                },
+                success: function(response) {
+                  alert(response);
+                }
+             });
+          });
+          // add the item to Wishlist
+          $('.Wishlist').click(function(){
+              var id = $(this).data('id');
+            // alert(id);
+            $.ajax({
+                url: 'main/wishlist?id='+ id,
+                method: 'post',
+                cache: false,
+                data: {
+                  id: id
+                },
+                success: function(response) {
+                  alert(response);
+                }
+             });
+          });
+})
+
+</script>
+
+  <br><br><br><br><br><br>
+  <br><br><br><br><br><br>
+
+  <br><br><br><br><br><br>
+
+<?PHP
+include "app/views/footer.php"; 
+?>
+    <script>
+      $("#sliderShuffle").cycle({
+        next: '#next',
+        prev: '#prev'
+      });
+      
+      $('.owl-carousel').owlCarousel({
+        items:4,
+        loop:true,
+        margin:15,
+        autoplay:true,
+        autoplayTimeout:3000, //3 Second
+        nav:true,
+        responsiveClass:true,
+        responsive:{
+          0:{
+            items:1,
+            nav:true
+          },
+          600:{
+            items:3,
+            nav:true
+          },
+          1000:{
+            items:4,
+            nav:true
+          }
+        }
+
+      });
+
+      $(function(){
+        $(".topbar ul li").click(function(){
+          $(".topbar ul li").not(this).find("ul").slideUp();
+          $(this).find("ul").slideToggle();
+        });
+        $(".topbar ul li ul li, productCategories ul li .megamenu").click(function(e){
+          e.stopPropagation();	
+        });
+        $(".productCategories ul li").click(function(){
+          $(".productCategories ul li").not(this).find(".megamenu").hide();
+          $(this).find(".megamenu").toggle();
+        });
+        $(".otherInfoBody").hide();
+        $(".otherInfoHandle").click(function(){
+          $(".otherInfoBody").slideToggle();
+        });
+        $(".signBtn").click(function(){
+          $("body").css("overflow", "hidden");
+          $(".loginBox").slideDown();
+        });
+        $(".closeBtn").click(function(){
+          $("body").css("overflow", "visible");
+          $(".loginBox").slideUp();
+        });
+        $(".productViewBtn").click(function(e){
+          e.preventDefault();
+          $("body").css("overflow", "hidden");
+          $(".productViewBox").slideDown();
+        });
+        $(".productViewBox-closeBtn").click(function(){
+          $("body").css("overflow", "visible");
+          $(".productViewBox").slideUp();
+        });
+
+        $(".sp-wrap").smoothproducts();
+      });
+    </script>
+      
+      <script>
+$(document).ready(function(){
+            // add the item to cart
+            $('.cart').click(function(){
+              var id = $(this).data('id');
+            $.ajax({
+                url: 'main/shopingCart?id='+ id,
+                method: 'post',
+                cache: false,
+                data: {
+                  id: id
+                },
+                success: function(response) {
+                  if(response == 0){
+                   alert("تم اضافة المنتج مسبقاً");
+                  }
+                  else{
+                   alert("تم إضافة المنتج الى السلة  ");
+                    document.getElementById("comparison-count").innerHTML=response;
+                    document.getElementById("shope").style.color = "blue";
+                  }
+                }
+             });
+          });
+          // add the item to Wishlist
+          $('.Wishlist').click(function(){
+              var id = $(this).data('id');
+            $.ajax({
+                url: 'main/wishlist?id='+ id,
+                method: 'post',
+                cache: false,
+                data: {
+                  id: id
+                },
+                success: function(response) {
+                  if(response == 0){
+                   alert("تم اضافة المنتج مسبقاً");
+                  }
+                  else{
+                   alert("تم إضافة المنتج الى المضلة  ");
+                    document.getElementById("comparison-count").innerHTML=response;
+                    document.getElementById("wishlisticon").style.color = "blue";
+                  }
+                }
+             });
+          });
+})
+</script>
+  
+  </body>
+</html>
